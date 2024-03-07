04aeed738f9e74ceab7ffd6241c4fff6ada67bf5
Joshua W. Kern <57602749+jwkern@users.noreply.github.com>
2024-03-07 13:25:40 -0600

    Update customerData_JWK.sql

--- customerData_JWK.sql
+++ customerData_JWK.sql
@@ -35,9 +35,9 @@ CREATE TABLE stores(
 );
 
 
-CREATE TABLE card_type(
+CREATE TABLE cardType(
         id INT,
-        card_type TEXT,
+        cardType TEXT,
         PRIMARY KEY (id)
 );
 
@@ -54,19 +54,19 @@ CREATE TABLE customers(
 );
 
 
-CREATE TABLE card_number(
+CREATE TABLE cardNumber(
         id INT,
-	card_number TEXT,
+	cardNumber TEXT,
 	customer_id INT,
-	card_type_id INT,
+	cardType_id INT,
 	PRIMARY KEY (id)
 );
 
 
-CREATE TABLE card_transactions(
+CREATE TABLE cardTransactions(
         id INT,
         date DATE,
-        card_number_id INT,
+        cardNumber_id INT,
         revenue FLOAT,
         cost FLOAT,
         PRIMARY KEY (id)
@@ -77,9 +77,9 @@ CREATE TABLE card_transactions(
 Step 2: Load the data into the tables
 ________________________________________________________________________________*/
 LOAD DATA LOCAL INFILE '/home/jwkern/Downloads/stores.csv' REPLACE INTO TABLE stores FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 rows;
-LOAD DATA LOCAL INFILE '/home/jwkern/Downloads/card_type.csv' REPLACE INTO TABLE card_type FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 rows;
-LOAD DATA LOCAL INFILE '/home/jwkern/Downloads/card_transactions.csv' REPLACE INTO TABLE card_transactions FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 rows;
-LOAD DATA LOCAL INFILE '/home/jwkern/Downloads/card_number.csv' REPLACE INTO TABLE card_number FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 rows;
+LOAD DATA LOCAL INFILE '/home/jwkern/Downloads/card_type.csv' REPLACE INTO TABLE cardType FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 rows;
+LOAD DATA LOCAL INFILE '/home/jwkern/Downloads/card_transactions.csv' REPLACE INTO TABLE cardTransactions FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 rows;
+LOAD DATA LOCAL INFILE '/home/jwkern/Downloads/card_number.csv' REPLACE INTO TABLE cardNumber FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 rows;
 LOAD DATA LOCAL INFILE '/home/jwkern/Downloads/customers.csv' REPLACE INTO TABLE customers FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 rows;
 
 
@@ -90,18 +90,18 @@ Step 3: Select subset of data and calculate properties
 _______________________________________________________________________________*/
 CREATE TABLE profitTransactions AS SELECT MONTH(date) AS month, 
 				YEAR(date) AS year, 
-				(revenue - cost) AS profit,
-				card_number.customer_id AS customer_id,
-				card_number.id AS card_id
-FROM card_transactions
-RIGHT JOIN card_number 
-ON card_transactions.card_number_id = card_number.id;
+				(revenue - cost)/revenue AS profit_margin,
+				cardNumber.customer_id AS customer_id,
+				cardNumber.id AS card_id
+FROM cardTransactions
+RIGHT JOIN cardNumber 
+ON cardTransactions.cardNumber_id = cardNumber.id;
 
 
 CREATE TABLE monthlyProfit AS 
 SELECT year,
        month,
-       ROUND(SUM(profit),2) AS profit
+       ROUND(SUM(profit_margin),2) AS monthly_profit_margin
 FROM profitTransactions
 GROUP BY year, month
 ORDER BY year, month;
@@ -113,13 +113,18 @@ SELECT EXTRACT(YEAR FROM date) AS year,
        COUNT(revenue) AS sales,
        ROUND(SUM(revenue),2) AS revenue,
        ROUND(SUM(cost),2) AS cost
-FROM card_transactions
+FROM cardTransactions
 GROUP BY EXTRACT(YEAR FROM date), EXTRACT(MONTH FROM date)
 ORDER BY EXTRACT(YEAR FROM date) ASC, EXTRACT(MONTH FROM date);
 
 
 CREATE TABLE monthlySummary AS
-SELECT monthlyTransactions.year, monthlyTransactions.month, monthlyTransactions.sales, monthlyTransactions.revenue, monthlyTransactions.cost, monthlyProfit.profit
+SELECT monthlyTransactions.year, 
+	monthlyTransactions.month, 
+	monthlyTransactions.sales, 
+	monthlyTransactions.revenue, 
+	monthlyTransactions.cost, 
+	monthlyProfit.monthly_profit_margin
 FROM monthlyTransactions
 RIGHT JOIN monthlyProfit
 ON monthlyTransactions.month = monthlyProfit.month;
